678c118e2eb6c35f51106f58c7f0a5655dba5d82
Krystian <krystianw.e.t@gmail.com>
2020-10-05 22:50:48 +0200

    created custom user as moderator2

--- events/models.py
+++ events/models.py
@@ -1,13 +1,18 @@
-from django.db import models
 import random
-from django.contrib.auth.models import User
+
+from django.db import models
+
 
 # Create your models here.
 class Event(models.Model):
-    #id
-    name = models.CharField(blank=True, null=True, unique=True, max_length=100)
-    description = models.CharField(blank=True, null=True, max_length=500)
-    text = models.CharField(blank=True, null=True, max_length=5000)
+    # id
+    name = models.CharField(blank=False, null=False, unique=True, max_length=100, verbose_name=("Nazwa wydarzenia"))
+    description = models.CharField(blank=False, null=False, max_length=500, verbose_name=("Opis wydarzenia"))
+    text = models.CharField(blank=False, null=False, max_length=5000, verbose_name=("Treść ogłoszenia eventu"))
+    # organizer as text at this moment
+    organizer = models.CharField(blank=False, null=False, max_length=100, verbose_name=("Organizator"))
+    eventDate = models.DateTimeField(blank=False, verbose_name=("Data Eventu"))
+    location = models.CharField(max_length=150, verbose_name=("Lokalizacja Eventu"))
 
     class Meta:
         ordering = ['-id']
@@ -18,6 +23,5 @@ class Event(models.Model):
             "name": self.name,
             "description": self.description,
             "text": self.text,
-            "likes": random.randint(0,200),
+            "likes": random.randint(0, 200),
         }
-
--- events/views.py
+++ events/views.py
@@ -1,55 +1,14 @@
-from django.conf import settings
-from django.http import JsonResponse
-from django.shortcuts import render, redirect
-from django.utils.http import is_safe_url
-
-from .forms import EventForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .serializer import EventSerializer
 from .models import Event
+#from .permissions import IsHighPermission
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
-ALLOWED_HOSTS = settings.ALLOWED_HOSTS
-
-# Create your views here.
-def home_viec(request, *args, **kwargs):
-    return render(request, "home.html")
-
-
-def event_create_view(request, *args, **kwargs):
-    form = EventForm(request.POST or None)
-    next_url = request.POST.get("next") or None
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse(obj.serialize(), status=201)
-        if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
-            return redirect(next_url)
-        form = EventForm()
-    if form.errors:
-        if request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-    return render(request, 'components/form.html', context={"form": form})
+class EventList(LoginRequiredMixin, generics.ListAPIView):
+    raise_exception = True
+    permission_classes = []
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 
-def events_list_view(request, *args, **kwargs):
-    querySet = Event.objects.all()
-    events_list = [x.serialize() for x in
-                   querySet]
-    data = {
-        "response": events_list
-    }
-    return JsonResponse(data)
-
-
-def event_detail_view(request, event_id, *args, **kwargs):
-    data = {
-        "id": event_id,
-    }
-    status = 200
-    try:
-        obj = Event.objects.get(id=event_id)
-        data['name'] = obj.name
-    except:
-        data['message'] = "Not Found"
-        status = 404
-
-    return JsonResponse(data, status=status)

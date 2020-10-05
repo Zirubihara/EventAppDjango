3d615fa0fd82430c738c28d0f90967d2e97c2422
Krystian <krystianw.e.t@gmail.com>
2020-10-05 15:35:51 +0200

    working on new admin panel

--- users/models.py
+++ users/models.py
@@ -4,4 +4,14 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    email = models.EmailField
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
 
